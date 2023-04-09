342c2c23c634a6671191356b1eedc2e28b650ff2
orsasson12 <orsasson72@gmail.com>
2023-04-09 13:48:51 +0300

    first commit

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 3000),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['qC/baQD8z9mCrT{@{L}', '=5w=TH{3Xg2Dv3.5`vN']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
