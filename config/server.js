501342e22b4f13b1c81432be3a457e3b20393855
orsasson12 <orsasson72@gmail.com>
2023-04-19 12:29:22 +0300

    commit

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'onrender.com'),
   port: env.int('PORT', 3000),
   app: {
     keys: env.array('APP_KEYS', ['qC/baQD8z9mCrT{@{L}', '=5w=TH{3Xg2Dv3.5`vN']),
